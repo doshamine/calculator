69a1292525b4df7b1a0b4aa8d23277e84daf1476
doshamine <sdmarch213@gmail.com>
2025-11-10 12:46:58 +0600

    added both cases

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,22 @@ public class Main {
         int b = calc.minus.apply(1,1);
         // ошибка: деление на ноль (b = 0)
         // Возможные решения:
-        // 1. Обработать исключение с помощью
-        //    конструкции try/except
-        // 2. Проверить значение делителя
+        // 1. Проверить значение делителя
         //    до выполнения операции и
         //    вывести сообщение в случае b = 0
+        if (b != 0) {
+            int c = calc.divide.apply(a, b);
+            calc.println.accept(c);
+        } else {
+            System.err.println("Был передан нулевой делитель");
+        }
+        // 2. Обработать исключение с помощью
+        //    конструкции try/except
         try {
             int c = calc.divide.apply(a, b);
             calc.println.accept(c);
         } catch (ArithmeticException e) {
-            System.out.println("Ошибка при делении: " + e.getMessage());
+            System.err.println("Ошибка при делении: " + e.getMessage());
         }
     }
 }
